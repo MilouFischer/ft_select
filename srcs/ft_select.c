7827c320734c7f570434949974c3d5d52545f8ca
Etienne FISCHER <efischer@e1r2p17.42.fr>
2019-09-03 16:42:07 +0200

    clear screen OK

--- srcs/ft_select.c
+++ srcs/ft_select.c
@@ -6,13 +6,48 @@
 /*   By: efischer <efischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 11:34:24 by efischer          #+#    #+#             */
-/*   Updated: 2019/09/03 15:24:54 by efischer         ###   ########.fr       */
+/*   Updated: 2019/09/03 16:40:41 by efischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_select.h"
 
-int		main(int ac, char **av)
+static int	put_termcap(int	termcap)
+{
+	int		ret;
+
+	ret = ft_putchar((char)termcap);
+	return (ret);
+}
+
+static void	cl_screen(void)
+{
+	char	*cl_cap;
+
+	if (tgetflag("cl") == SUCCESS)
+	{
+		cl_cap = tgetstr("cl", NULL);
+		tputs(cl_cap, 1, put_termcap);
+	}
+}
+
+static void	ft_select(char **av)
+{
+	char	buf[BUF_SIZE];
+	int		ret;
+
+	while (1)
+	{
+		cl_screen();
+		ft_print_tab(av);
+		tputs(tgetstr("\E", NULL), 1, put_termcap);
+		if ((ret = read(0, &buf, BUF_SIZE)) >= 0)
+			buf[ret] = '\0';
+		ft_putendl(buf);
+	}
+}
+
+int			main(int ac, char **av)
 {
 	int		ret;
 
@@ -21,9 +56,8 @@ int		main(int ac, char **av)
 		ft_putendl_fd("usage: ft_select ...", 2);
 		exit(EXIT_FAILURE);
 	}
-	if ((ret = init_entry()) != TRUE)
-		return (ret);
-	while (1)
-		ft_print_tab(av + 1);
-	return (EXIT_SUCCESS);
+	ret = init_entry();
+	if (ret == TRUE)
+		ft_select(av + 1);
+	return (ret);
 }
